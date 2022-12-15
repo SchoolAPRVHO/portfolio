a1cd11226cd054d76ff0fbed60b86bc5c536d830
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-15 17:39:45 +0100

    updated wit the npm command

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,41 @@ import modernTheme from "./themes/modern";
 import greenTheme from "./themes/green";
 import Apis from "./components/api/Apis";
 import Api from "./components/Api";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
 
 const App = () => {
   const [theme, setTheme] = useState(greenTheme);
-  const toggleTheme = () => {
-    setTheme(theme === orangeTheme ? modernTheme : orangeTheme);
+  const toggleTheme = (theme: any) => {
+    const themes = [greenTheme, orangeTheme, modernTheme];
+
+    switch (theme) {
+      case greenTheme:
+        setTheme(orangeTheme);
+        break;
+      case orangeTheme:
+        setTheme(modernTheme);
+        break;
+      case modernTheme:
+        setTheme(greenTheme);
+        break;
+    }
   };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Nav />
-      <Button title="Toggle Theme" onClick={toggleTheme} style={{width:"fit-content"}}></Button>
+      <Button
+        title="Toggle Theme"
+        onClick={() => toggleTheme(theme)}
+        style={{ width: "fit-content" }}
+      >
+        Change colors <ColorLensIcon />
+      </Button>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/showcase"  >
-          <Route index element={<Showcase />}/>
+        <Route path="/showcase">
+          <Route index element={<Showcase />} />
           <Route path=":showcaseId" element={<Showcases />} />
         </Route>
         <Route path="/api">
--- src/components/showcases/Timer/TimeContainer.tsx
+++ src/components/showcases/Timer/TimeContainer.tsx
@@ -2,16 +2,18 @@ import CurrentTime from "./CurrentTime";
 import Timer from "./Timer";
 import RandomValue from "./RandomValue";
 import styles from './TimeContainer.module.css';
+import { Container } from "@mui/material";
 
 
 const TimeContainer = ()=>{
     return (
         <>
-            <div className={styles.container}>
+            <Container className={styles.container}>
+                <h1>Timer component</h1>
                 <Timer interval={1000}/>
                 <CurrentTime interval={1}/>
                 <RandomValue interval={1000}/>
-            </div>
+            </Container>
         </>
     )
 }
